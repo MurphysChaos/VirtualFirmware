9110e67061f31b025ae7c98615d00b4d8c3c237d
Joel Murphy <joel@murphyschaos.com>
2011-07-01 13:34:05 -0700

    The renamed sockerr files.

---
+++ sockets/tinytalk/sockerr.c
@@ -0,0 +1,17 @@
+/* sock_error -- Simplifies log output for string-based errors.
+ *
+ * Copyright (C) 2011 Jacob Keller, All rights reserved
+ * The code in this file is licensed under GPL v2. This license does not 
+ * extend to any file bundled with this one.
+ * * * */
+
+#include "sockerr.h"
+
+const char* sock_error() {
+  int errval = errno;
+#ifdef _WIN32
+  return wsa_strerror(WSAGetLastError());
+#else
+  return strerror(errval);
+#endif
+}

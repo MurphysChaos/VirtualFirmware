1e2f500e42f79f71a009dfb7cddea63a6e86e1db
Joel Murphy <joel@murphyschaos.com>
2011-07-11 20:10:17 -0700

    First draft of options stuff.

---
+++ sockets/api/opt.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define OPTRC "optrc"
+#define OPTBUFLEN 1024
+
+/*
+ * A structure for handling options.
+ */
+
+struct {
+    char mcastip[16];  // IP only, URL not allowed
+    char mcastport[6];
+    uint32_t magicnum; // Magic number for broadcast packets
+    char tcpport[6];
+} OPT;
+
+void ReadOptions();

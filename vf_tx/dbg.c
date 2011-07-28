67d3e8a9b676cdc50d24728cc117673fd9345fe2
Jacob Keller <jacob.keler@gmail.com>
2011-07-27 20:14:45 -0700

    Modified dbg.c to add a time string before messages.
    Changed dbg to a variadic macro in order for less code duplication.
    fdbg now prints the time before the message itself

--- vf_tx/dbg.c
+++ vf_tx/dbg.c
@@ -10,24 +10,21 @@
 */
 
 #include "dbg.h"
-#include "opt.h"
 
 void fdbg(FILE * f, int level, const char *msg, ...)
 {
     if (OPT.dbglvl >= level) {
         va_list args;
+	time_t t;
+	
+	/* Print the time stamp */
+	time(&t);
+	fprintf(f, "%s:", ctime(&t)); 
+
+	/* Print the rest of the message */
         va_start(args, msg);
         vfprintf(f, msg, args);
         va_end(args);
     }
 }
 
-void dbg(int level, const char *msg, ...)
-{
-    if (OPT.dbglvl >= level) {
-        va_list args;
-        va_start(args, msg);
-        vfprintf(stderr, msg, args);
-        va_end(args);
-    }
-}
--- vf_tx/dbg.h
+++ vf_tx/dbg.h
@@ -14,13 +14,18 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <time.h>
+#include "opt.h"
 
 #define DBG_STATUS 1
 #define DBG_ERROR  2
 #define DBG_WARN   3
 #define DBG_VERB   4
 
+#define dbg(level, msg, ...) \
+    fdbg(stderr, level, msg, ##__VA_ARGS__);
+
 void fdbg(FILE * f, int level, const char *msg, ...);
-void dbg(int level, const char *msg, ...);
+
 
 #endif
